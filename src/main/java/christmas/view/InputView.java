fe4658bdb2c113e65bfe4e3a24aa0f6c9a0493e5
김민규 <alsrbtls88@gmail.com>
2023-12-08 09:46:57 +0900

    feat: 날짜 입력 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,12 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.exception.ExceptionCode;
+import christmas.utils.converter.Converter;
+import christmas.view.constant.Notice;
+import christmas.view.dto.OrderDto;
+import java.util.List;
+
 public class InputView {
     private static final InputView instance = new InputView();
 
@@ -8,6 +15,18 @@ public class InputView {
 
     public static InputView getInstance() {
         return instance;
+    }
+
+    public int readReserveDate() {
+        Notice.DATE.print();
+
+        return Converter.toInt(
+                Console.readLine(),
+                ExceptionCode.INVALID_DATE
+        );
+    }
+
+    public List<OrderDto> readOrders() {
     }
 
 }
